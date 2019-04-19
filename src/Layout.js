37c624c1c4152f3cb0987838b8cf3fbe83e8d578
Adrian de la Rosa <adrian@delarosab.me>
2019-04-19 17:11:16 +0200

    :art: Improving calculation of Layout

--- src/Layout.js
+++ src/Layout.js
@@ -1,13 +1,22 @@
-import React, { Component, createContext, useContext } from 'react';
+import React, { Component, createContext, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
+// We want to include this little function in our own bundle
+// eslint-disable-next-line import/no-extraneous-dependencies
+import memoize from 'underscore-es/memoize';
 
 const Context = createContext({});
 const { Provider } = Context;
 
-const useLayout = ({ height, name, position, width }) => {
-  const { canvas, setComponent } = useContext(Context);
+const useLayout = ({ height = 0, name, position = 'center', width = 0 }) => {
+  if (!name) {
+    throw new Error('hello');
+  }
 
-  setComponent(name, position, { height, width });
+  const { canvas, setComponentInPosition } = useContext(Context);
+
+  useEffect(() => {
+    setComponentInPosition({ height, name, width }, position);
+  }, [name, position, height, width]);
 
   return canvas[name] || { height: 0, width: 0, x: 0, y: 0 };
 };
@@ -22,17 +31,121 @@ class Layout extends Component {
     width: PropTypes.number,
   };
 
+  static size(components = [], isPilingUp = false) {
+    return components.reduce(
+      (acc, component) => ({
+        height: isPilingUp
+          ? acc.height + component.height
+          : Math.max(acc.height, component.height),
+        width: isPilingUp
+          ? Math.max(acc.width, component.width)
+          : acc.width + component.width,
+      }),
+      { height: 0, width: 0 },
+    );
+  }
+
   components = { bottom: {}, center: {}, left: {}, right: {}, top: {} };
 
   state = {
     canvas: {},
   };
 
-  setComponent = (name, position, size) => {
+  calculate = memoize(
+    components => {
+      const { height: SVGHeight, width: SVGWidth } = this.props;
+
+      const regions = Object.entries(components).reduce(
+        (acc, [position, componentMap]) => {
+          const isStacking = position === 'bottom' || position === 'top';
+
+          return {
+            ...acc,
+            [position]: Layout.size(Object.values(componentMap), isStacking),
+          };
+        },
+        {},
+      );
+      const { bottom, center, left, right, top } = components;
+
+      return {
+        ...Object.entries(bottom).reduce(
+          (acc, [name, size], index) => ({
+            ...acc,
+            [name]: {
+              height: size.height,
+              width: SVGWidth - regions.left.width - regions.right.width,
+              x: regions.left.width,
+              y: Layout.size(Object.values(bottom).slice(0, index), true)
+                .height,
+            },
+          }),
+          {},
+        ),
+        ...Object.entries(center).reduce(
+          (acc, [name]) => ({
+            ...acc,
+            [name]: {
+              height: SVGHeight - regions.top.height - regions.bottom.height,
+              width: SVGWidth - regions.top.height - regions.bottom.height,
+              x: regions.left.width,
+              y: regions.bottom.height,
+            },
+          }),
+          {},
+        ),
+        ...Object.entries(left).reduce(
+          (acc, [name, size], index) => ({
+            ...acc,
+            [name]: {
+              height: SVGHeight - regions.top.height - regions.bottom.height,
+              width: size.width,
+              x: Layout.size(Object.values(left).slice(0, index), false).width,
+              y: regions.bottom.height,
+            },
+          }),
+          {},
+        ),
+        ...Object.entries(right).reduce(
+          (acc, [name, size], index) => ({
+            ...acc,
+            [name]: {
+              height: SVGHeight - regions.top.height - regions.bottom.height,
+              width: size.width,
+              x:
+                SVGWidth -
+                size.width -
+                Layout.size(Object.values(right).slice(0, index), false).width,
+              y: regions.bottom.height,
+            },
+          }),
+          {},
+        ),
+        ...Object.entries(top).reduce(
+          (acc, [name, size], index) => ({
+            ...acc,
+            [name]: {
+              height: size.height,
+              width: SVGWidth - regions.left.width - regions.right.width,
+              x: regions.left.width,
+              y:
+                SVGHeight -
+                size.height -
+                Layout.size(Object.values(top).slice(0, index), true).height,
+            },
+          }),
+          {},
+        ),
+      };
+    },
+    (...argv) => JSON.stringify(argv),
+  );
+
+  setComponentInPosition = ({ name, ...size }, position) => {
     this.setState(() => {
       const prevComponents = this.components;
 
-      const components = {
+      this.components = {
         ...prevComponents,
         [position]: {
           ...prevComponents[position],
@@ -40,98 +153,18 @@ class Layout extends Component {
         },
       };
 
-      return { canvas: this.paintCanvas(components) };
+      return { canvas: this.calculate(this.components) };
     });
   };
 
-  paintCanvas(components) {
-    const { height, width } = this.props;
-
-    const padding = Object.entries(components).reduce(
-      (acc, [position, component]) => ({
-        ...acc,
-        [position]: Object.values(component).reduce(
-          (result, size) => result + size,
-          0,
-        ),
-      }),
-      {},
-    );
-    const { bottom, center, left, right, top } = components;
-
-    return {
-      ...Object.entries(bottom).reduce((acc, [name, size], index) => ({
-        ...acc,
-        [name]: {
-          height: size,
-          width: width - padding.left - padding.right,
-          x: padding.left,
-          y: Object.entries(bottom).reduce(
-            (racc, [, rsize], rindex) => racc + (rindex < index ? rsize : 0),
-            0,
-          ),
-        },
-      })),
-      ...Object.entries(center).reduce((acc, [name]) => ({
-        ...acc,
-        [name]: {
-          height: height - padding.top - padding.bottom,
-          width: width - padding.top - padding.bottom,
-          x: padding.left,
-          y: padding.bottom,
-        },
-      })),
-      ...Object.entries(left).reduce((acc, [name, size], index) => ({
-        ...acc,
-        [name]: {
-          height: height - padding.top - padding.bottom,
-          width: size,
-          x: Object.entries(left).reduce(
-            (racc, [, rsize], rindex) => racc + (rindex < index ? rsize : 0),
-            0,
-          ),
-          y: padding.bottom,
-        },
-      })),
-      ...Object.entries(right).reduce((acc, [name, size], index) => ({
-        ...acc,
-        [name]: {
-          height: height - padding.top - padding.bottom,
-          width: size,
-          x:
-            width -
-            size -
-            Object.entries(right).reduce(
-              (racc, [, rsize], rindex) => racc + (rindex < index ? rsize : 0),
-              0,
-            ),
-          y: padding.bottom,
-        },
-      })),
-      ...Object.entries(top).reduce((acc, [name, size], index) => ({
-        ...acc,
-        [name]: {
-          height: size,
-          width: width - padding.left - padding.right,
-          x: padding.left,
-          y:
-            height -
-            size -
-            Object.entries(top).reduce(
-              (racc, [, rsize], rindex) => racc + (rindex < index ? rsize : 0),
-              0,
-            ),
-        },
-      })),
-    };
-  }
-
   render() {
-    const { setComponent } = this;
+    const { setComponentInPosition } = this;
     const { children } = this.props;
     const { canvas } = this.state;
 
-    return <Provider value={{ canvas, setComponent }}>{children}</Provider>;
+    return (
+      <Provider value={{ canvas, setComponentInPosition }}>{children}</Provider>
+    );
   }
 }
 
