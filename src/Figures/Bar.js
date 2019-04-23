55b270f467cd1f2a9fc683470ffdf7113d79fe69
Adrian de la Rosa <adrian@delarosab.me>
2019-04-23 12:57:17 +0200

    :recycle: Refactoring Bar to use previous height

--- src/Figures/Bar.js
+++ src/Figures/Bar.js
@@ -1,56 +1,50 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Animation, Rect } from 'calvin-svg';
 
-class Bar extends Component {
-  static defaultProps = {
-    duration: 500,
-    ease: 'cubic-out',
-    height: 0,
-    radius: 4,
-    width: 8,
-  };
+function Bar({
+  children,
+  delay,
+  duration = 500,
+  ease = 'cubic-out',
+  height = 0,
+  radius = 4,
+  width = 8,
+  ...props
+}) {
+  const prevHeight = useRef(0);
 
-  static propTypes = {
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node,
-    ]),
-    delay: PropTypes.number,
-    duration: PropTypes.number,
-    ease: PropTypes.string,
-    height: PropTypes.number,
-    radius: PropTypes.number,
-    width: PropTypes.number,
-  };
+  useEffect(() => {
+    prevHeight.current = height;
+  }, [height]);
 
-  render() {
-    const { prevHeight } = this;
-    const {
-      children,
-      delay,
-      duration,
-      ease,
-      height,
-      width,
-      ...props
-    } = this.props;
-
-    return (
-      <Rect width={width} {...props}>
-        <Animation
-          attribute="height"
-          delay={delay}
-          duration={duration}
-          ease={ease}
-          fill="freeze"
-          from={prevHeight}
-          to={height}
-        />
-        {children}
-      </Rect>
-    );
-  }
+  return (
+    <Rect radius={radius} width={width} {...props}>
+      <Animation
+        attribute="height"
+        delay={delay}
+        duration={duration}
+        ease={ease}
+        fill="freeze"
+        from={prevHeight.current}
+        to={height}
+      />
+      {children}
+    </Rect>
+  );
 }
+
+Bar.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+  delay: PropTypes.number,
+  duration: PropTypes.number,
+  ease: PropTypes.string,
+  height: PropTypes.number,
+  radius: PropTypes.number,
+  width: PropTypes.number,
+};
 
 export default Bar;
