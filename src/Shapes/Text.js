95792e34be594f81483c3476556ef64f811e5c0a
Adrian de la Rosa <adrian@delarosab.me>
2019-04-24 16:24:21 +0200

    :fire: Removing extra braces

--- src/Shapes/Text.js
+++ src/Shapes/Text.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Text as Shape } from 'calvin-svg';
 
-const Text = props => {
-  return <Shape {...props} transform="scale(1, -1)" />;
-};
+const Text = props => <Shape {...props} transform="scale(1, -1)" />;
 
 export default Text;
