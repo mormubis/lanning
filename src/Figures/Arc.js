c9240fa1988d9f507ba9d1fc36f4eede30898d5f
Adrian de la Rosa <adrian@delarosab.me>
2019-04-24 16:22:36 +0200

    :recycle: Refactoring Arc Figure

--- src/Figures/Arc.js
+++ src/Figures/Arc.js
@@ -1,82 +1,78 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Animation, Arc as Shape } from 'calvin-svg';
 
-class Arc extends Component {
-  static defaultProps = {
-    duration: 500,
-    ease: 'cubic-out',
-    thickness: 15,
-  };
+const Arc = ({
+  children,
+  delay,
+  duration = 500,
+  ease = 'cubic-out',
+  endAngle,
+  height,
+  radius,
+  startAngle,
+  thickness = 15,
+  width,
+  ...props
+}) => {
+  const prevEndAngle = useRef(startAngle);
+  const prevStartAngle = useRef(startAngle);
 
-  static propTypes = {
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node,
-    ]),
-    delay: PropTypes.number,
-    duration: PropTypes.number,
-    endAngle: PropTypes.number.isRequired,
-    ease: PropTypes.string,
-    height: PropTypes.number,
-    startAngle: PropTypes.number.isRequired,
-    thickness: PropTypes.number,
-    width: PropTypes.number,
-  };
+  useEffect(() => {
+    prevEndAngle.current = endAngle;
+    prevStartAngle.current = startAngle;
+  }, [endAngle, startAngle]);
 
-  prevEndAngle = this.props.startAngle;
+  return (
+    <Shape
+      endAngle={prevEndAngle.current}
+      height={height}
+      radius={radius}
+      startAngle={prevStartAngle.current}
+      thickness={thickness}
+      width={width}
+      {...props}
+    >
+      <Animation
+        attribute="d"
+        delay={delay}
+        duration={duration}
+        ease={ease}
+        fill="freeze"
+        step={percentage =>
+          Shape.d({
+            endAngle:
+              prevEndAngle.current +
+              (endAngle - prevEndAngle.current) * percentage,
+            thickness,
+            height,
+            radius,
+            startAngle:
+              prevStartAngle.current +
+              (startAngle - prevStartAngle.current) * percentage,
+            width,
+          })
+        }
+      />
+      {children}
+    </Shape>
+  );
+};
 
-  prevStartAngle = this.props.startAngle;
-
-  componentDidUpdate(prevProps) {
-    this.prevEndAngle = prevProps.endAngle;
-    this.prevStartAngle = prevProps.startAngle;
-  }
-
-  render() {
-    const { prevEndAngle, prevStartAngle } = this;
-    const {
-      children,
-      delay,
-      duration,
-      ease,
-      endAngle,
-      height,
-      startAngle,
-      thickness,
-      width,
-      ...props
-    } = this.props;
-
-    return (
-      <Shape
-        endAngle={endAngle}
-        height={height}
-        startAngle={startAngle}
-        thickness={thickness}
-        width={width}
-        {...props}
-      >
-        <Animation
-          attribute="d"
-          delay={delay}
-          duration={duration}
-          ease={ease}
-          step={percentage =>
-            Arc.d({
-              endAngle: prevEndAngle + (prevEndAngle - endAngle) * percentage,
-              thickness,
-              height,
-              startAngle:
-                prevStartAngle + (prevStartAngle - startAngle) * percentage,
-              width,
-            })
-          }
-        />
-        {children}
-      </Shape>
-    );
-  }
-}
+Arc.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+  delay: PropTypes.number,
+  duration: PropTypes.number,
+  endAngle: PropTypes.number.isRequired,
+  ease: PropTypes.string,
+  height: PropTypes.number,
+  radius: PropTypes.number,
+  startAngle: PropTypes.number.isRequired,
+  thickness: PropTypes.number,
+  width: PropTypes.number,
+};
 
 export default Arc;
