0d4ff37e5ff42c0c2447af8d140bb49e8f2d984a
Adrian de la Rosa <adrian@delarosab.me>
2019-04-24 16:26:02 +0200

    :recycle: Refactoring Pie

--- src/Series/Pie.js
+++ src/Series/Pie.js
@@ -1,64 +1,77 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Layer } from 'calvin-svg';
 
 import Arc from '../Figures/Arc';
-import Scales from '../Scales';
+import Scale from '../Scale';
 import Serie from '../Serie';
 
 export const Pie = ({
-  color,
-  data: raw,
-  delay,
-  duration,
-  height,
-  padAngle,
-  width,
+  colors = ['#222222'],
+  data: raw = [],
+  delay = 0,
+  duration = 3000,
+  padAngle = 3,
+  radius = 4,
+  thickness = 8,
   ...props
 }) => {
-  const scale = Scales.linear();
-  const MAX = Math.PI * 2;
-  const OFFSET = Math.PI / 2;
+  const MAX = 360;
+  const OFFSET = 180;
+
+  const total = raw.reduce((acc, slice) => acc + slice, 0);
 
   return (
-    <Serie {...props} scales={[scale]} sizes={[MAX]}>
-      {data =>
-        data.map(([startAngle], index) => {
-          const size = (data[index + 1] || MAX) - startAngle;
+    <>
+      <Scale domain={[0, 1]} name="pie" />
 
-          return (
-            <Arc
-              delay={(duration / data.length) * index}
-              duration={duration / data.length}
-              endAngle={startAngle + size - padAngle + OFFSET}
-              height={height}
-              startAngle={startAngle + OFFSET}
-              width={width}
-            />
-          );
-        })
-      }
-    </Serie>
+      <Serie
+        {...props}
+        data={raw.map(slice => [slice / total])}
+        scales={['pie']}
+        ranges={[MAX]}
+      >
+        {({ data, height, width }) => (
+          <Layer transform="scale(-1, 1)" x={width}>
+            {data
+              .map(([size]) => size)
+              .map((size, index, array) => {
+                const startAngle = array[index - 1] || 0;
+                // skipping linter (we do not have any other unmutable value
+                const position = index;
+
+                return (
+                  <Arc
+                    color={colors[index % colors.length]}
+                    delay={delay + (duration / data.length) * index}
+                    duration={duration / data.length}
+                    endAngle={startAngle + size - padAngle + OFFSET}
+                    height={height}
+                    key={position}
+                    radius={radius}
+                    startAngle={startAngle + OFFSET}
+                    thickness={thickness}
+                    width={width}
+                    x={width / 2}
+                    y={height / 2}
+                  />
+                );
+              })}
+          </Layer>
+        )}
+      </Serie>
+    </>
   );
 };
 
-Pie.defaultProps = {
-  color: '#222222',
-  data: [],
-  delay: 0,
-  duration: 3000,
-  height: 0,
-  padAngle: 0,
-  width: 0,
-};
-
 Pie.propTypes = {
-  color: PropTypes.string,
+  colors: PropTypes.arrayOf(PropTypes.string),
   data: PropTypes.arrayOf(PropTypes.number),
   delay: PropTypes.number,
   duration: PropTypes.number,
-  height: PropTypes.number,
   padAngle: PropTypes.number,
-  width: PropTypes.number,
+  radius: PropTypes.number,
+  thickness: PropTypes.number,
 };
 
 export default Pie;
