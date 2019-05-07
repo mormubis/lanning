ef70c59675f56cb9a81a15fce2763627335f6aea
Adrian de la Rosa <adrian@delarosab.me>
2019-05-07 16:56:01 +0200

    :bug: Fixing Text position

--- src/Shapes/Text.js
+++ src/Shapes/Text.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Text as Shape } from 'calvin-svg';
 
-const Text = props => <Shape {...props} transform="scale(1, -1)" />;
+const Text = ({ y, ...props }) => (
+  <Shape y={-y} {...props} transform="scale(1, -1)" />
+);
+
+Text.propTypes = {
+  y: PropTypes.number,
+};
 
 export default Text;
