013d1b60f62e1cdf379a9a96b94476a2630005f9
Adrian de la Rosa <adrian@delarosab.me>
2019-05-08 17:44:03 +0200

    :art: Adding ref to Text

--- src/Shapes/Text.js
+++ src/Shapes/Text.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import { Text as Shape } from 'calvin-svg';
 
-const Text = ({ y, ...props }) => (
-  <Shape y={-y} {...props} transform="scale(1, -1)" />
+const Text = ({ forwardedRef, y = 0, ...props }) => (
+  <Shape y={-y} {...props} ref={forwardedRef} transform="scale(1, -1)" />
 );
 
 Text.propTypes = {
+  forwardedRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  ]),
   y: PropTypes.number,
 };
 
-export default Text;
+export default forwardRef((props, ref) => (
+  <Text {...props} forwardedRef={ref} />
+));
