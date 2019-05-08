59452d5da64e8900c07e5392e6e6c2780ba9cc13
Adrian de la Rosa <adrian@delarosab.me>
2019-05-08 10:59:24 +0200

    :recycle: Refactoring Line to functional component

--- src/Figures/Line.js
+++ src/Figures/Line.js
@@ -1,91 +1,82 @@
-import React, { Component, createRef } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Animation, Line as Shape } from 'calvin-svg';
 
-export class Line extends Component {
-  static defaultProps = {
-    duration: 3000,
-    ease: 'cubic-out',
-    opacity: 1,
-    thickness: 3,
-  };
+const Line = ({
+  children,
+  curve,
+  delay,
+  duration = 3000,
+  ease = 'cubic-out',
+  opacity = 1,
+  points,
+  thickness = 3,
+  ...props
+}) => {
+  const element = useRef(null);
+  const prevPoints = useRef(points);
+  const [length, setLength] = useState(0);
+  const hash = JSON.stringify(points);
 
-  static propTypes = {
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node,
-    ]),
-    curve: PropTypes.string,
-    delay: PropTypes.number,
-    duration: PropTypes.number,
-    ease: PropTypes.string,
-    opacity: PropTypes.number,
-    points: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
-    thickness: PropTypes.number,
-  };
+  useEffect(() => {
+    prevPoints.current = points;
+  }, [hash]);
 
-  element = createRef();
+  useLayoutEffect(() => {
+    setLength(element.current.getTotalLength());
+  }, []);
 
-  prevPoints = this.props.points;
+  return (
+    <Shape
+      ref={element}
+      strokeDasharray={length}
+      points={points}
+      {...props}
+      opacity={length === 0 ? 0 : opacity}
+      thickness={thickness}
+    >
+      <Animation
+        attribute="stroke-dashoffset"
+        duration={duration}
+        ease={ease}
+        from={length}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        to={0}
+      />
+      <Animation
+        attribute="d"
+        delay={delay}
+        duration={duration}
+        ease={ease}
+        step={percentage =>
+          Shape.d({
+            curve,
+            points: points.map(
+              (value, index) =>
+                prevPoints.current[index] +
+                (value - prevPoints.current[index]) * percentage,
+            ),
+          })
+        }
+      />
+      {children}
+    </Shape>
+  );
+};
 
-  state = { length: 0 };
-
-  componentDidMount() {
-    const length = this.element.current.getTotalLength();
-
-    this.setState({ length });
-  }
-
-  render() {
-    const { prevPoints } = this;
-    const {
-      children,
-      curve,
-      delay,
-      duration,
-      ease,
-      opacity,
-      points,
-      ...props
-    } = this.props;
-    const { length } = this.state;
-
-    return (
-      <Shape
-        ref={this.element}
-        strokeDasharray={length}
-        points={points}
-        {...props}
-        opacity={length === 0 ? 0 : opacity}
-      >
-        <Animation
-          attribute="stroke-dashoffset"
-          duration={duration}
-          ease={ease}
-          from={length}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          to={0}
-        />
-        <Animation
-          attribute="d"
-          delay={delay}
-          duration={duration}
-          ease={ease}
-          step={percentage =>
-            Shape.d({
-              curve,
-              points: points.map(
-                (value, index) =>
-                  prevPoints[index] + (value - prevPoints[index]) * percentage,
-              ),
-            })
-          }
-        />
-        {children}
-      </Shape>
-    );
-  }
-}
+Line.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+  curve: PropTypes.string,
+  delay: PropTypes.number,
+  duration: PropTypes.number,
+  ease: PropTypes.string,
+  opacity: PropTypes.number,
+  points: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
+  thickness: PropTypes.number,
+};
 
 export default Line;
