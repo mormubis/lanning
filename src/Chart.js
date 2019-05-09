5cc7d99e36839254b5542e41d929ce34d0115b26
Adrian de la Rosa <adrian@delarosab.me>
2019-05-09 14:27:15 +0200

    :recycle: Refactoring Chart to functional component

--- src/Chart.js
+++ src/Chart.js
@@ -1,4 +1,10 @@
-import React, { Component, createContext, useContext } from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useRef,
+  useState,
+} from 'react';
 import PropTypes from 'prop-types';
 import { Layer, SVG } from 'calvin-svg';
 import uuid from 'uuid/v4';
@@ -35,79 +41,52 @@ const useScale = ({ name, range }) => {
   return useScales({ scales: [name], ranges: [range] })[0];
 };
 
-class Chart extends Component {
-  static defaultProps = {
-    bottom: 0,
-    left: 0,
-    right: 0,
-    top: 0,
-  };
+const Chart = ({
+  bottom = 0,
+  children,
+  height,
+  left = 0,
+  right = 0,
+  top = 0,
+  width,
+  ...props
+}) => {
+  const id = useRef(uuid());
+  const [scales, setScales] = useState({});
 
-  static propTypes = {
-    bottom: PropTypes.number,
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node,
-    ]),
-    height: PropTypes.number,
-    left: PropTypes.number,
-    right: PropTypes.number,
-    top: PropTypes.number,
-    width: PropTypes.number,
-  };
-
-  state = { scales: {} };
-
-  id = uuid();
-
-  setScale = (name, value) => {
-    this.setState(prevState => ({
-      scales: { ...prevState.scales, [name]: value },
+  const setScale = useCallback((name, value) => {
+    setScales(prevState => ({
+      ...prevState,
+      [name]: value,
     }));
-  };
+  }, []);
 
-  render() {
-    const { setScale } = this;
-    const {
-      bottom,
-      children,
-      height,
-      left,
-      right,
-      top,
-      width,
-      ...props
-    } = this.props;
-    const { scales } = this.state;
-
-    return (
-      <SVG height={height} width={width} {...props}>
-        <Layer
-          transform={`scale(1, -1) translate(${left}, ${-height + bottom})`}
-        >
-          <Provider value={{ scales, setScale }}>
-            <OverlayProvider value={this.id}>
-              <Layout
-                height={height - top - bottom}
-                width={width - left - right}
-              >
-                {children}
-                <Overlay />
-              </Layout>
-            </OverlayProvider>
-          </Provider>
-        </Layer>
-      </SVG>
-    );
-  }
-}
+  return (
+    <SVG height={height} width={width} {...props}>
+      <Layer transform={`scale(1, -1) translate(${left}, ${-height + bottom})`}>
+        <Provider value={{ scales, setScale }}>
+          <OverlayProvider value={id.current}>
+            <Layout height={height - top - bottom} width={width - left - right}>
+              {children}
+              <Overlay />
+            </Layout>
+          </OverlayProvider>
+        </Provider>
+      </Layer>
+    </SVG>
+  );
+};
 
 Chart.propTypes = {
+  bottom: PropTypes.number,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
   height: PropTypes.number.isRequired,
+  left: PropTypes.number,
+  right: PropTypes.number,
+  top: PropTypes.number,
   width: PropTypes.number.isRequired,
 };
 
