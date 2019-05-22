46c3c5349b29a975e25c1e76049729ffc94130c1
Adrian de la Rosa <adrian@delarosab.me>
2019-05-22 18:18:13 +0200

    :recycle: Using new tooltip behavior

--- src/Series/Bar.js
+++ src/Series/Bar.js
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import randomColor from 'random-color';
 // We want to include this little function in our own bundle
@@ -13,34 +13,48 @@ export const Bar = ({
   data: raw = [],
   delay = 0,
   duration = 3000,
-  tooltip: onTooltip = v => v,
+  tooltip: transform = (x, y) => y,
   ...props
 }) => {
-  const handleTarget = memoize((tooltip, value) => ({ shape }) => {
-    const [x, y] = shape.centroid;
+  const handleOver = useCallback(
+    ({ shape }, index, onOver) => {
+      const [x, y] = shape.centroid;
 
-    return tooltip.open({ color, message: onTooltip(value), x, y });
+      onOver({ color, index, transform, x, y });
+    },
+    [color, transform],
+  );
+
+  const handleTarget = memoize((index, onOver) => (...argv) => {
+    handleOver(...argv, index, onOver);
   });
 
-  return (
-    <Serie {...props} data={raw.map((value, index) => [index, value])}>
-      {({ data, height, tooltip, width }) => {
-        return data.map(([position, size], index) => (
+  const children = useCallback(
+    memoize(
+      ({ data, height, onOver, onOut, width }) =>
+        data.map(([position, size], index) => (
           <Shape
             color={color}
             delay={delay + ((duration / 2) * index) / (data.length - 1)}
             duration={duration / 2}
             height={size}
             key={`${position},${width}x${height}`}
-            onBlur={tooltip.close}
-            onFocus={handleTarget(tooltip, raw[index])}
-            onMouseOut={tooltip.close}
-            onMouseOver={handleTarget(tooltip, raw[index])}
-            x={position - 4}
+            onBlur={onOut}
+            onFocus={handleTarget(index, onOver)}
+            onMouseOut={onOut}
+            onMouseOver={handleTarget(index, onOver)}
+            x={position}
             y={0}
           />
-        ));
-      }}
+        )),
+      (...argv) => JSON.stringify(argv),
+    ),
+    [color, delay, duration],
+  );
+
+  return (
+    <Serie {...props} data={raw.map((value, index) => [index, value])}>
+      {children}
     </Serie>
   );
 };
