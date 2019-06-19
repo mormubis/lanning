cf489432d7758a5c2946be1bb8d73f1d46d23208
Adrian de la Rosa <adrian@delarosab.me>
2019-06-19 15:57:20 +0200

    :ambulance: Fixing dist path

--- index.module.js
+++ index.module.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./es/lanning.production.min.js');
+  module.exports = require('./es/lanning.production.js');
 } else {
   module.exports = require('./es/lanning.development.js');
 }
