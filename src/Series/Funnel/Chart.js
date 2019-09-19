17bf3c9932fe68f4969c2346607d01e14b181e72
Adrian de la Rosa <adrian@delarosab.me>
2019-09-19 11:02:00 +0200

    :recycle: Refactoring Funnel Chart

---
+++ src/Series/Funnel/Chart.js
@@ -0,0 +1,109 @@
+import React, { memo, useCallback, useMemo } from 'react';
+import { Polygon } from 'calvin-svg';
+import PropTypes from 'prop-types';
+import randomColor from 'randomcolor';
+
+import Area from '../../Figures/Area';
+import { memoize } from '../../utils';
+
+const mirror = (data = [], offset = 0, height = 0) =>
+  data.map(([x, rawY]) => {
+    const h0 = height / 2;
+    const h1 = h0 - offset;
+    const y0 = rawY / 2;
+    const y1 = (h1 / h0) * y0 + offset;
+
+    return [x, y1, -y1];
+  });
+
+const Chart = ({
+  color = randomColor(),
+  curve = 'monotone-x',
+  delay = 0,
+  domain = [],
+  duration = 3000,
+  height,
+  onOut = () => {},
+  onOver = () => {},
+  onTooltip = () => {},
+  range = [],
+  width,
+}) => {
+  const gaps = domain.length - 1;
+  const offset = height * 0.0125; // 2.5% of the height
+  const mirrored = mirror(range, offset, height);
+
+  const handleOver = useCallback(
+    ({ shape }, index) => {
+      const { centroid, x, y } = shape;
+
+      onOver({
+        color,
+        index,
+        shape,
+      });
+
+      onTooltip({ color, index, x: centroid[0] + x, y: centroid[1] + y });
+    },
+    [JSON.stringify(domain)],
+  );
+
+  const createOverHandler = useMemo(
+    () => memoize(index => (...argv) => handleOver(...argv, index)),
+    [handleOver],
+  );
+
+  return (
+    <>
+      <Area
+        color={color}
+        curve={curve}
+        delay={delay}
+        duration={duration}
+        key={`${width}x${height}`}
+        points={mirrored}
+        x={0}
+        y={height / 2}
+      />
+      {Array.from({ length: gaps }).map((ignore, index) => {
+        const key = `hover:${index}`;
+        const points = [
+          [mirrored[index][0], mirrored[index][1]],
+          [mirrored[index + 1][0], mirrored[index + 1][1]],
+          [mirrored[index + 1][0], mirrored[index + 1][2]],
+          [mirrored[index][0], mirrored[index][2]],
+        ];
+
+        return (
+          <Polygon
+            color="transparent"
+            key={key}
+            onBlur={onOut}
+            onFocus={createOverHandler(index + 1)}
+            onMouseOut={onOut}
+            onMouseOver={createOverHandler(index + 1)}
+            points={points}
+            x={0}
+            y={height / 2}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+Chart.propTypes = {
+  color: PropTypes.string,
+  curve: PropTypes.string,
+  delay: PropTypes.number,
+  domain: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)),
+  duration: PropTypes.number,
+  height: PropTypes.number,
+  onOut: PropTypes.func,
+  onOver: PropTypes.func,
+  onTooltip: PropTypes.func,
+  range: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)),
+  width: PropTypes.number,
+};
+
+export default memo(Chart);
---
+++ src/Series/Funnel/index.js
@@ -0,0 +1,64 @@
+import React, { memo, useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+import Serie from '../../Serie';
+import Scale from '../../Scale';
+import { memoize } from '../../utils';
+
+import Chart from './Chart';
+
+const Funnel = ({
+  color,
+  curve,
+  data: domain = [],
+  delay,
+  duration,
+  ...props
+}) => {
+  const steps = Array.from({ length: domain.length }, (_, index) => index);
+  const target = domain[0] || 1;
+
+  const children = useMemo(
+    () =>
+      memoize(
+        context => (
+          <Chart
+            color={color}
+            curve={curve}
+            delay={delay}
+            domain={domain}
+            duration={duration}
+            {...context}
+          />
+        ),
+        (...argv) => JSON.stringify(argv),
+      ),
+    [color, curve, delay, duration],
+  );
+
+  return (
+    <>
+      <Scale domain={steps} name="steps" type="point" />
+      <Scale domain={[0, target]} name="target" />
+
+      <Serie
+        {...props}
+        data={domain.map((step, index) => [index, step])}
+        scales={['steps', 'target']}
+      >
+        {children}
+      </Serie>
+    </>
+  );
+};
+
+Funnel.propTypes = {
+  color: PropTypes.string,
+  curve: PropTypes.string,
+  data: PropTypes.arrayOf(PropTypes.number),
+  delay: PropTypes.number,
+  duration: PropTypes.number,
+  tooltip: PropTypes.func,
+};
+
+export default memo(Funnel);
